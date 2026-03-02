559df8041e3dc45d81d49e07bd226dbc66f1851c
ELCOT <nidhusiva05@gmail.com>
2026-03-02 16:15:12 +0530

    Edit delete session

--- MyProject/MyApp/urls.py
+++ MyProject/MyApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DepartmentListCreate, CourseListCreate, InstructorListCreate, StudentListCreate, ClassroomListCreate, EnrollmentListCreate
+from .views import DepartmentListCreate, CourseListCreate, InstructorListCreate, StudentListCreate, ClassroomListCreate, EnrollmentListCreate, EnrollmentDetail, DepartmentDetail, CourseDetail, InstructorDetail, StudentDetail, ClassroomDetail
 
 urlpatterns = [
     path('departments/', DepartmentListCreate.as_view(), name='department-list'),
@@ -8,10 +8,10 @@ urlpatterns = [
     path('students/', StudentListCreate.as_view(), name='student-list'),
     path('classrooms/', ClassroomListCreate.as_view(), name='classroom-list'),
     path('enrollments/', EnrollmentListCreate.as_view(), name='enrollment-list'),
-    path('departments/<str:dep_code>/', DepartmentListCreate.as_view(), name='department-detail'),
-    path('courses/<str:course_code>/', CourseListCreate.as_view(), name='course-detail'),
-    path('instructors/<int:instructor_id>/', InstructorListCreate.as_view(), name='instructor-detail'),
-    path('students/<int:s_id>/', StudentListCreate.as_view(), name='student-detail'),
-    path('classrooms/<int:crn>/', ClassroomListCreate.as_view(), name='classroom-detail'),
-    path('enrollments/<int:pk>/', EnrollmentListCreate.as_view(), name='enrollment-detail'),
+    path('departments/<str:dep_code>/', DepartmentDetail.as_view(), name='department-detail'),
+    path('courses/<str:course_code>/', CourseDetail.as_view(), name='course-detail'),
+    path('instructors/<int:instructor_id>/', InstructorDetail.as_view(), name='instructor-detail'),
+    path('students/<int:s_id>/', StudentDetail.as_view(), name='student-detail'),
+    path('classrooms/<int:crn>/', ClassroomDetail.as_view(), name='classroom-detail'),
+    path('enrollments/<int:pk>/', EnrollmentDetail.as_view(), name='enrollment-detail'),
 ]
